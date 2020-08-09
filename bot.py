87785e09d11edeee15c02391cee2565da4e3bfff
Steven <stevencm199gmail.com>
2020-08-08 18:02:54 -0600

    a

--- bot.py
+++ bot.py
@@ -16,12 +16,6 @@ client = commands.Bot(command_prefix='.')
 #    txtChnl = client.get_channel(737234405348605996)
 #    await txtChnl.send("hola amiguito")
 
-#@client.event
-#async def on_disconnect():
-#    txtChnl = client.get_channel(741416524274729021)
-#    await txtChnl.send("adios amiguito")
-
-
 @client.event
 async def on_member_join(member):
     print(f'{member} ha entrado al server')
@@ -37,16 +31,12 @@ async def on_voice_state_update(member, before, after):
     datetime_CR = datetime.now(tz_CR)
     channel = client.get_channel(741416524274729021)
 
-    # JUGADOR SE UNE AL CANAL
-    if before.voice.voice_channel is None and after.voice.voice_channel is not None:
+    if before.channel is None and after.channel is not None:
         await channel.send(f'{member} se ha unido al canal {after.channel.name} a las {datetime_CR.strftime("%H:%M:%S")}')
-
-    # JUGADOR SE SALE DEL CANAL
-    if after.voice.voice_channel is None and before.voice.voice_channel is not None:
+    if after.channel is None and before.channel is not None:
         await channel.send(f'{member} ha salido del canal {before.channel.name} a las {datetime_CR.strftime("%H:%M:%S")}')
 
-    # JUGADOR CAMBIA DE CANAL
-    if before.voice.voice_channel is not None and after.voice.voice_channel is not None:
+    if before.channel != None and after.channel != None:
         await channel.send(f'{member} se ha movido al canal {after.channel.name} a las {datetime_CR.strftime("%H:%M:%S")}')
 
 
