0e51d1a9f702b7a46154aaf5eb54b263f73b80f5
Steven <stevencm199gmail.com>
2021-07-01 13:39:51 -0600

    logChannel

--- bot.py
+++ bot.py
@@ -4,17 +4,30 @@ import sys
 from datetime import datetime
 import pytz
 import random
+from discord import guild
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='.')
-
 #txtChnl = client.get_channel(737234405348605996)
 
 #@client.event
 #async def on_ready():
-#    print("estoy ready")
+   # global logChannel
 #    txtChnl = client.get_channel(737234405348605996)
 #    await txtChnl.send("hola amiguito")
+
+@client.command()
+async def add(ctx, a: int, b: int):
+    channels = ["comandos"]
+    if str(ctx.channel) in channels:
+        await ctx.send(a + b)
+
+@client.command()
+async def logChannel(ctx, logs):
+    global logChannel
+    logChannel = discord.utils.get(ctx.guild.channels, name=logs)
+    await ctx.send(logChannel)
+   # print (logChannel)
 
 @client.event
 async def on_member_join(member):
@@ -27,19 +40,23 @@ async def on_member_remove(member):
 
 @client.event #Detecta si alguien ha entrado a un canal
 async def on_voice_state_update(member, before, after):
-    tz_CR = pytz.timezone('America/Costa_Rica')
-    datetime_CR = datetime.now(tz_CR)
-    channel = client.get_channel(741416524274729021)
+   # print(logChannel)
 
-    if before.channel is None and after.channel is not None:
-        await channel.send(f' el mamapichas de {member} se ha unido al canal {after.channel.name} a las {datetime_CR.strftime("%H:%M:%S")}, que risa ah?')
-    if after.channel is None and before.channel is not None:
-        await channel.send(f' el mamapichas de {member} ha salido del canal {before.channel.name} a las {datetime_CR.strftime("%H:%M:%S")}, que risa ah?')
+    for channel in member.guild.channels:
+        if str(channel) == str(logChannel):
+            tz_CR = pytz.timezone('America/Costa_Rica')
+            datetime_CR = datetime.now(tz_CR)
+            #channel = client.get_channel(741416524274729021)
 
-    
+            if before.channel is None and after.channel is not None:
+                await channel.send(
+                    f' el mamapichas de {member} se ha unido al canal {after.channel.name} a las {datetime_CR.strftime("%H:%M:%S")}, que risa ah?')
+            if after.channel is None and before.channel is not None:
+                await channel.send(
+                    f' el mamapichas de {member} ha salido del canal {before.channel.name} a las {datetime_CR.strftime("%H:%M:%S")}, que risa ah?')
 
 @client.command()
-async def clear(ctx, amount=5):
+async def clear(ctx, amount=1):
     await ctx.channel.purge(limit=amount)
 
 
@@ -57,7 +74,8 @@ async def clear(ctx, amount=5):
 
 @client.command()
 async def hoy(ctx):
-    now = datetime.datetime.now()
+    tz_CR = pytz.timezone('America/Costa_Rica')
+    now = datetime.datetime.now(tz_CR)
     await ctx.send("Dia y Hora actuales:")
     await ctx.send(now.strftime("%Y-%m-%d %H:%M:%S"))
 
