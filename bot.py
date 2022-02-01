0d3fa7ef534c4688830f29d7abb6da9e18e2ecc7
StevenCM199 <stevencm199@gmail.com>
2022-02-01 00:57:17 -0600

    listaconectados

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import discord
 from datetime import datetime
 import pytz
+import schedule as schedule
 from discord.ext import commands
 import time
 
 client = commands.Bot(command_prefix='.')
-conectados = [] #lista de conectados
-dc_time = []
+conectados = []  # lista de conectados
 uptime = datetime
+
 
 def convert(seconds):
     if seconds < 60:
@@ -15,12 +16,14 @@ def convert(seconds):
     elif seconds < 3600:
         return time.strftime("%M minutos, %S segundos", time.gmtime(seconds))
     elif seconds > 3600:
-        return time.strftime("%H Horas %M minutos y %S segundos", time.gmtime(seconds))
+        return time.strftime("%H horas %M minutos y %S segundos", time.gmtime(seconds))
+
 
 @client.event
 async def on_ready():
     global logChannel
-    logChannel = "paynorbot" #canal de logs
+    logChannel = "paynorbot"  # canal de logs
+
 
 @client.command()
 async def logChannel(ctx, logs):
@@ -31,7 +34,6 @@ async def logChannel(ctx, logs):
 
 @client.event  # Detecta si alguien ha entrado a un canal
 async def on_voice_state_update(member, before, after):
-
     for channel in member.guild.channels:
 
         if str(channel) == str(logChannel):
@@ -40,21 +42,17 @@ async def on_voice_state_update(member, before, after):
 
             if before.channel is None and after.channel is not None:  # conectarse a voz
                 conectados.append([member.name, datetime_CR])
-                print(conectados)
+                #print(conectados)
                 await channel.send(
                     f' :white_check_mark: {member.name} se unió al canal {after.channel.name} a las {datetime_CR.strftime("%I:%M %p")}')
 
             if after.channel is None and before.channel is not None:  # desconectarse de voz
-                dc_time.append([member.name, datetime_CR])
-                print(dc_time)
                 for i in reversed(range(len(conectados))):
-                    if conectados[i][0] == member.name and dc_time[i][0] == member.name:
-                        uptime = dc_time[i][1] - conectados[i][1]
+                    if conectados[i][0] == member.name:
+                        uptime = datetime_CR - conectados[i][1]
                         seconds = uptime.total_seconds()
                         del conectados[i]
-                        del dc_time[i]
-                print(conectados)
-                print(dc_time)
+                #print(conectados)
 
                 await channel.send(
                     f' :x: {member.name} salió del canal {before.channel.name} a las {datetime_CR.strftime("%I:%M %p")}')
@@ -63,5 +61,20 @@ async def on_voice_state_update(member, before, after):
                 seconds = None
 
 
+@client.command()
+async def clearLists(ctx):
+    conectados.clear()
+    await ctx.send('=========Lista vaciada=========')
 
+
+@client.command()
+async def listaconectados(ctx):
+    if conectados == []:
+        await ctx.send("Lista vacia")
+    else:
+        for i in reversed(range(len(conectados))):
+            await ctx.send(conectados[i])
+
+
+schedule.every().day.at("3:30:00").do(clearLists())
 client.run('NzM3MjQ0MTQ0MDI3Njk3MTg1.Xx6iHQ.dNgQ-ivU51IMgmqljDTqZrU5J_Y')
